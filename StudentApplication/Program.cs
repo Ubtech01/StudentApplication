36084e0ccef9b691cd669a7f3241a6eaca049ccd
Mr. Ubong <127354903+Ubtech01@users.noreply.github.com>
2023-06-25 22:55:42 +0100

    Add project files.

---
+++ StudentApplication/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace StudentApplication
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+	
+			
+
+			int studentId = 0;
+			string firstName = string.Empty;
+			string lastName = string.Empty;
+			decimal loanAmount = 0;
+			char gender;
+			bool isNew;
+
+            Console.WriteLine("Enter student Id");
+			studentId  = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter student first name");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter student last name");
+            lastName = Console.ReadLine();
+
+			Console.WriteLine("Please enter the student loan amount");
+			loanAmount = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter the student gender");
+			gender = Convert.ToChar(Console.ReadLine());
+
+			Console.WriteLine("The student is new (true/fasle)");
+			isNew = Convert.ToBoolean(Console.ReadLine());	
+
+			Student stu = new Student(studentId, firstName, lastName, loanAmount, gender, isNew);
+
+           
+
+        }
+	}
+}
---
+++ StudentApplication/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication
+{
+	public class Student
+	{
+		int _studentId = 0;
+		string _firstName = string.Empty;
+		string _lastName = string.Empty;
+		decimal _loanAmount = 0;
+		char _gender;
+		bool _isNew;
+
+
+        public Student(int studentId, string firstName, string lastName, decimal loanAmount, char gender, bool isNew)
+        {
+            _studentId = studentId;
+			_firstName = firstName;
+			_lastName = lastName;
+			_loanAmount = loanAmount;
+			_gender = gender;
+			_isNew = isNew;
+        }
+
+		public void UpdateLoanAmount(decimal loanAmount) 
+		{
+		   _loanAmount = loanAmount;
+		}
+
+		public string StudentData()
+		{
+			string StudentData = $"studentId {_studentId}, first name {_firstName}, loan amount {_loanAmount}";
+			return StudentData ;	
+		}
+    }
+}
